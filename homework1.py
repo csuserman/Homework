fe61b18f97c8f0bed535486e419ccf9c29cf8859
csuserman <math_cs@icloud.com>
2024-10-25 19:51:05 +0500

    homework1

---
+++ homework1.py
@@ -0,0 +1,257 @@
+import math
+#begin1
+#a = float(input("a = "))
+#print("P = ", 4 * a)
+
+#begin2
+#a = float(input("a = "))
+#print("S = ", a ** 2)
+
+#begin3
+#a = float(input("a = "))
+#b = float(input("b = "))
+#print("P = ", 2 * (a + b))
+#print("S = ", a * b)
+
+#begin4
+#d = float(input("d = "))
+#pi = 3.14
+#print("L = ", pi * d)
+
+#begin5
+#a = float(input("a = "))
+#print("V = ", a ** 3)
+#print("S = ", 6 * a ** 2)
+
+#begin6
+#a = float(input("a = "))
+#b = float(input("b = "))
+#c = float(input("c = "))
+#print("V = ", a * b * c)
+#print("S = ", 2 * (a * b + b * c + c *a))
+
+#begin7
+#R = float(input("R = "))
+#pi = 3.14
+#print("L = ", 2 * pi * R)
+#print("S = ", pi * R ** 2)
+
+#begin8
+#a = float(input("a = "))
+#b = float(input("b = "))
+#print("arithmetic mean = ", (a + b)/2)
+
+#begin9
+#a = float(input("Enter non-negative a: "))
+#b = float(input("Enter non-negative b: "))
+#print("geometric mean = ", math.sqrt(a * b))
+
+#begin10
+#a = float(input("Enter non-zero a: "))
+#b = float(input("Enter non-zero b: "))
+#print("a + b = ", a + b)
+#print("a * b = ", a * b)
+#print("a^2 = ", a ** 2)
+#print("b^2 = ", b ** 2)
+
+#begin11
+#a = float(input("Enter non-zero a: "))
+#b = float(input("Enter non-zero b: "))
+#print("a + b = ", a + b)
+#print("a * b = ", a * b)
+#print("|a| = ", math.sqrt(a ** 2))
+#print("|b| = ", math.sqrt(b ** 2))
+
+#begin12
+#a = float(input("a = "))
+#b = float(input("b = "))
+#c = math.sqrt(a ** 2 + b ** 2)
+#print("c = ", c)
+#print("P = ", a + b + c)
+
+#begin13
+#R1 = float(input("R1 = "))
+#R2 = float(input("R2 = "))
+#pi = 3.14
+#print("R3 = ", pi * math.sqrt((R1 - R2) ** 2))4
+
+#begin14
+#L = float(input("L = ")) 
+#pi = 3.14
+#R = L/(2 * pi)
+#print("R = ", R)
+#print("S = ", pi * R ** 2)
+
+#begin15
+#S = float(input("S = "))
+#pi = 3.14
+#R = math.sqrt(S/pi)
+#print("d = ", 2 * R)
+#print("R = ", R)
+
+#begin16
+#x1 = float(input("x1 = "))
+#x2 = float(input("x2 = "))
+#print("|x1 - x2| = ", ((x1 - x2) ** 2) ** 0.5)
+
+#begin17
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#AB = ((A - B) ** 2) ** 0.5
+#BC = ((B - C) ** 2) ** 0.5
+#CA = ((C - A) ** 2) ** 0.5
+#print("AC = ", CA)
+#print("BC = ", BC)
+#print("Total = ", AB + BC + CA)
+
+#begin18
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#BC = ((B - C) ** 2) ** 0.5
+#CA = ((C - A) ** 2) ** 0.5
+#print("AC * BC = ", BC * CA)
+
+#begin19
+#x1 = float(input("x1 = "))
+#y1 = float(input("y1 = "))
+#x2 = float(input("x2 = "))
+#y2 = float(input("y2 = "))
+#print("P = ", 2 * ((((x1 - x2) ** 2) ** 0.5) + (((y1 - y2) ** 2) ** 0.5)))
+#print("S = ", (((x1 -x2) * (y1 - y2)) ** 2) ** 0.5)
+
+#begin20
+#x1 = float(input("x1 = "))
+#y1 = float(input("y1 = "))
+#x2 = float(input("x2 = "))
+#y2 = float(input("y2 = "))
+#print("distance = ", ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5)
+
+#begin21
+#x1 = float(input("x1 = "))
+#y1 = float(input("y1 = "))
+#x2 = float(input("x2 = "))
+#y2 = float(input("y2 = "))
+#x3 = float(input("x3 = "))
+#y3 = float(input("y3 = "))
+#a = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+#b = ((x2 - x3) ** 2 + (y2 - y3) ** 2) ** 0.5
+#c = ((x3 - x1) ** 2 + (y3 - y1) ** 2) ** 0.5
+#P = a + b + c
+#print("P = ", P)
+#print("S = ", (P/2 * (P/2-a) * (P/2-b) * (P/2-c)) ** 0.5)
+
+#begin22
+#A = float(input("A = "))
+#B = float(input("B = "))
+#print("A = ", B)
+#print("B = ", A)
+
+#begin23
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#print("A = ", C)
+#print("B = ", A)
+#print("C = ", B)
+
+#begin24
+#A = float(input("A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#print("A = ", B)
+#print("B = ", C)
+#print("C = ", A)
+
+#begin25
+#x = float(input("x = "))
+#print("result = ", 3 * x ** 6 - 6 * x ** 2 - 7)
+
+#begin26
+#x = float(input("x = "))
+#print("result = ", 4 * (x - 3) ** 6 - 7 * (x - 3) ** 3 + 2)
+
+#begin27
+#A = float(input("A = "))
+#print("A^2 = ", A ** 2)
+#print("A^4 = ", A ** 4)
+#print("A^8 = ", A ** 8)
+
+#begin28
+#A = float(input("A = "))
+#print("A^2 = ", A ** 2)
+#print("A^3 = ", A ** 3)
+#print("A^5 = ", A ** 5)
+#print("A^10 = ", A ** 10)
+#print("A^15 = ", A ** 15)
+
+#begin29
+#alpha = float(input("alpha = "))
+#print("Radian = ", alpha * math.pi/180)
+
+#begin30
+#radian = float(input("radian = "))
+#print("alpha = ", radian * 180/math.pi)
+
+#begin31
+#temp = float(input("Temperature Farengeyt: "))
+#print("Temperature gradus selsey = ", (temp - 32) * 5/9)
+
+#begin33
+#X = float(input("X = "))
+#A = float(input("A = "))
+#Y = float(input("Y = "))
+#print("1 kg konfet ", A/X, "so'm turadi")
+#print(Y, " kg konfet ", A/X * Y, "so'm turadi")
+
+#begin34
+#X = float(input("X = "))
+#A = float(input("A = "))
+#Y = float(input("Y = "))
+#B = float(input("B = "))
+#print("1 kg shokolad 1 kg konfetdan ", A/X - B/Y, " so'm qimmat turadi")
+
+#begin35
+#V = float(input("V = "))
+#U = float(input("U = "))
+#T1 = float(input("T1 = "))
+#T2 = float(input("T2 = "))
+#print("S = ", T1 * (V + U) + T2 * (V - U))
+
+#begin36
+#V1 = float(input("V1 = "))
+#V2 = float(input("V2 = "))
+#S = float(input("S = "))
+#T = float(input("T = "))
+#print("distance = ", S + (V1 + V2) * T)
+
+#begin37
+#V1 = float(input("V1 = "))
+#V2 = float(input("V2 = "))
+#S = float(input("S = "))
+#T = float(input("T = "))
+#print("distance = ", S - (V1 + V2) * T)
+
+#begin38
+#A = float(input("Enter non-zero A = "))
+#B = float(input("B = "))
+#print("x = ", -B/A)
+
+#begin39
+#A = float(input("Enter non-zero A = "))
+#B = float(input("B = "))
+#C = float(input("C = "))
+#D = B ** 2 - 4 * A * C
+#print("x1 = ", (-B + D ** 0.5)/(2 * A))
+#print("x2 = ", (-B - D ** 0.5)/(2 * A))
+
+#begin40
+#A1 = float(input("A1 = "))
+#B1 = float(input("A1 = "))
+#C1 = float(input("A1 = "))
+#A2 = float(input("A1 = "))
+#B2 = float(input("A1 = "))
+#C2 = float(input("A1 = "))
+#print("x = ", (C1 * B2 - C2 * B1)/(A1 * B2 - A2 * B1))
+#print("y = ", (A1 * C2 - A2 * C1)/(A1 * B2 - A2 * B1))
